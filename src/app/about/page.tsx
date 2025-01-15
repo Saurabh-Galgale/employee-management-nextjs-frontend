2a1a6b1ca95b970c7133982b87cadf4f3db0e9a7
Saurabh Galgale <110832790+Saurabh-Galgale@users.noreply.github.com>
2025-01-15 22:57:27 +0530

    cleaned about page and added section component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,179 +1,142 @@
 import React from "react";
-import {
-  Typography,
-  Box,
-  List,
-  ListItem,
-  ListItemText,
-  Divider,
-  Link,
-} from "@mui/material";
+import { Typography, Box, Divider, Link } from "@mui/material";
 import { GitHub, LinkedIn, Language } from "@mui/icons-material";
 
+// Reusable Typography Section Component
+const Section = ({ title, children }) => (
+  <>
+    <Typography
+      variant="h5"
+      sx={{ marginBottom: 1, color: "text.primary", textAlign: "center" }}
+    >
+      {title}
+    </Typography>
+    {children}
+    <Divider sx={{ margin: "10px 0" }} />
+  </>
+);
+
+// Reusable Skill/Achievement Component
+const ListItemTextContent = ({ content }) => (
+  <Typography
+    variant="body1"
+    sx={{ color: "text.primary", textAlign: "center" }}
+  >
+    {content}
+  </Typography>
+);
+
 const AboutPage = () => {
+  const skills = [
+    "Frontend Development: React.js, Vue.js, and modern UI libraries like Material-UI, Element Plus and Tailwind CSS.",
+    "State Management: Redux, RTK and Context API using composables for efficient app state handling.",
+    "Data Management: Expertise in integrating APIs using Axios, fetching, transforming (using transformer functions), and rendering complex datasets.",
+    "Performance Optimization: Minimizing loops, optimizing algorithms, and reducing time complexity in applications.",
+    "Backend Development: Node.js, Express.js, and REST/GraphQL API design.",
+  ];
+
+  const achievements = [
+    "Designed and implemented a multi-module production planning system with dynamic state management.",
+    "Developed advanced filtering and search features with optimized performance for large datasets.",
+  ];
+
+  const links = [
+    {
+      href: "https://github.com/Saurabh-Galgale",
+      icon: <GitHub sx={{ fontSize: "40px", color: "text.primary" }} />,
+    },
+    {
+      href: "https://www.linkedin.com/in/saurabh-galgale/",
+      icon: <LinkedIn sx={{ fontSize: "40px", color: "text.primary" }} />,
+    },
+    {
+      href: "https://saurabhgalgale.vercel.app/",
+      icon: <Language sx={{ fontSize: "40px", color: "text.primary" }} />,
+    },
+  ];
+
   return (
-    <>
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        minHeight: "90vh",
+        maxHeight: "fit content",
+        width: "full",
+        marginTop: 2,
+      }}
+    >
       <Box
         sx={{
-          display: "flex",
-          justifyContent: "center",
-          minHeight: "90vh",
-          maxHeight: "fit content",
-          width: "full",
-          marginTop: 2,
+          width: "90%",
+          padding: "20px",
+          borderRadius: "12px",
+          boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+          backgroundColor: "secondary.main",
         }}
       >
-        <Box
+        <Typography
+          variant="h4"
           sx={{
-            width: "90%",
-            padding: "20px",
-            borderRadius: "12px",
-            boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
-            backgroundColor: "secondary.main",
+            marginBottom: 2,
+            color: "text.primary",
+            textAlign: "center",
           }}
         >
-          <Typography
-            variant="h5"
-            sx={{
-              marginBottom: 2,
-              color: "text.primary",
-              textAlign: "center",
-            }}
-          >
-            Hi, I'm SAURABH GALGALE.
-          </Typography>
-
-          <Typography
-            variant="body1"
-            sx={{
-              marginBottom: "16px",
-              color: "text.primary",
-              textAlign: "center",
-            }}
-          >
-            I am a software engineer with over 3.5 years of experience in
-            building robust, scalable, and intuitive web applications. My
-            expertise lies in modern web technologies, crafting seamless user
-            experiences, and delivering high-quality solutions that align with
-            client needs.
-          </Typography>
-
-          <Divider sx={{ margin: "10px 0" }} />
-
-          <Typography
-            variant="h6"
-            sx={{ marginBottom: 1, color: "text.primary", textAlign: "center" }}
-          >
-            Core Skills
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            Frontend Development: React.js, Vue.js, and modern UI libraries like
-            Material-UI, Element Plus and Tailwind css.
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            State Management: Redux, RTK and Context API using composables for
-            efficient app state handling.
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            Data Management: Expertise in integrating APIs using Axios,
-            fetching, transforming (using transformer functions), and rendering
-            complex datasets.
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            Performance Optimization: Minimizing loops, optimizing algorithms,
-            and reducing time complexity in applications.
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            Backend Development: Node.js, Express.js, and REST/GraphQL API
-            design.
-          </Typography>
-
-          <Divider sx={{ margin: "10px 0" }} />
-
-          <Typography
-            variant="h6"
-            sx={{ marginBottom: 1, color: "text.primary", textAlign: "center" }}
-          >
-            Notable Achievements
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            Designed and implemented a multi-module production planning system
-            with dynamic state management.
-          </Typography>
-          <Typography
-            variant="body1"
-            sx={{ color: "text.primary", textAlign: "center" }}
-          >
-            Developed advanced filtering and search features with optimized
-            performance for large datasets.
-          </Typography>
-
-          <Typography
-            variant="body1"
-            sx={{
-              marginTop: "16px",
-              marginBottom: "20px",
-              textAlign: "center",
-            }}
-          >
-            My commitment to coding excellence is backed by my focus on
-            continuous learning and adopting best practices in software
-            development.
-          </Typography>
-
-          <Divider sx={{ margin: "10px 0" }} />
-
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "center",
-              gap: "20px",
-              marginTop: "30px",
-            }}
-          >
-            <Link
-              href="https://github.com/Saurabh-Galgale"
-              target="_blank"
-              color="inherit"
-            >
-              <GitHub sx={{ fontSize: "40px", color: "text.primary" }} />
+          Hi, I'm SAURABH GALGALE.
+        </Typography>
+        <Typography
+          variant="body1"
+          sx={{
+            marginBottom: "16px",
+            color: "text.primary",
+            textAlign: "center",
+          }}
+        >
+          I am a software engineer with over 3.5 years of experience in building
+          robust, scalable, and intuitive web applications. My expertise lies in
+          modern web technologies, crafting seamless user experiences, and
+          delivering high-quality solutions that align with client needs.
+        </Typography>
+        <Divider sx={{ margin: "10px 0" }} />
+        <Section title="Core Skills">
+          {skills.map((skill, index) => (
+            <ListItemTextContent key={index} content={skill} />
+          ))}
+        </Section>
+        <Section title="Notable Achievements">
+          {achievements.map((achievement, index) => (
+            <ListItemTextContent key={index} content={achievement} />
+          ))}
+        </Section>
+        <Typography
+          variant="body1"
+          sx={{
+            marginTop: "16px",
+            marginBottom: "20px",
+            textAlign: "center",
+          }}
+        >
+          My commitment to coding excellence is backed by my focus on continuous
+          learning and adopting best practices in software development.
+        </Typography>
+        <Divider sx={{ margin: "10px 0" }} />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "20px",
+            marginTop: "30px",
+          }}
+        >
+          {links.map((link, index) => (
+            <Link key={index} href={link.href} target="_blank" color="inherit">
+              {link.icon}
             </Link>
-            <Link
-              href="https://www.linkedin.com/in/saurabh-galgale/"
-              target="_blank"
-              color="inherit"
-            >
-              <LinkedIn sx={{ fontSize: "40px", color: "text.primary" }} />
-            </Link>
-            <Link
-              href="https://saurabhgalgale.vercel.app/"
-              target="_blank"
-              color="inherit"
-            >
-              <Language sx={{ fontSize: "40px", color: "text.primary" }} />
-            </Link>
-          </Box>
+          ))}
         </Box>
       </Box>
-    </>
+    </Box>
   );
 };
 
